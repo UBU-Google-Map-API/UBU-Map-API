d5503305747e228195fc8da3656ea39b21a991e9
athip60 <athip.si.60@ubu.ac.th>
2021-06-10 13:07:07 +0700

    add agm bootstrap & some component

--- ubu-map/src/app/app.component.ts
+++ ubu-map/src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ubu-map';
+  lat = 51.678418;
+  lng = 7.809007;
+
+  public ngOnInit(): void {
+    this.getLocation();
+  }
+
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position: Position) => {
+          if (position) {
+            console.log(
+              'Latitude: ' +
+                position.coords.latitude +
+                'Longitude: ' +
+                position.coords.longitude
+            );
+            this.lat = position.coords.latitude;
+            this.lng = position.coords.longitude;
+            console.log(this.lat);
+            console.log(this.lat);
+          }
+        },
+        (error: PositionError) => console.log(error)
+      );
+    } else {
+      alert('Geolocation is not supported by this browser.');
+    }
+  }
 }
--- ubu-map/src/app/app.module.ts
+++ ubu-map/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AgmCoreModule } from '@agm/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapPageComponent } from './pages/map-page/map-page.component';
+import { LayoutComponent } from './layouts/layout/layout.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, MapPageComponent, LayoutComponent, LoginComponent, RegisterComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyByxAxM_f64MaGCNie0y3joCJmUUA_TRZE',
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
